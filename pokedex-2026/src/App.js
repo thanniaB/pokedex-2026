cab2308aab1f932f76d186f3b59405499eb5a8d3
tblanchet <thannia.blanchet@points.com>
2026-09-18 16:07:26 -0400

    2: agregar funcion que settea el nombre

--- pokedex-2026/src/App.js
+++ pokedex-2026/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  // antes del return va lo que es programación como tal, vaya, las funciones y eso
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchboxChange = (event) => {
+    setSearchText(event.target.value);
+  }
+  // en el return usamos JSX, que es como HTML dentro de JS
   return (
+    // JSX solo soporta enviar un solo elemento, así que todo debe de estar dentro de un fragment, como mínimo
     <>
       <header>
         Pokedexito 2026        
       </header>
-      <input type="text" placeholder="escribe el nombre del pokemon aquí"></input>
+      <label for="pokemon">Escribe el nombre del pokemon aquí</label>
+      <input type="text" placeholder="eevee" name="pokemon" onChange={handleSearchboxChange}></input>
     </>
   );
 }
