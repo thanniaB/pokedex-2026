bf6b03f7b4ad637e9234aaf54a55dfa50e5c01e8
tblanchet <thannia.blanchet@points.com>
2026-09-18 16:50:11 -0400

    3: fetch y debounce

--- pokedex-2026/src/App.js
+++ pokedex-2026/src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect, useMemo} from 'react';
+import debounce from "debounce";
 
 function App() {
   // antes del return va lo que es programación como tal, vaya, las funciones y eso
   const [searchText, setSearchText] = useState("");
+  const DEBOUNCE_TIME_MILLISECONDS = 500;
 
   const handleSearchboxChange = (event) => {
     setSearchText(event.target.value);
   }
+
+  const fetchPokemon = async (searchTerm) => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+        const responseJson = await response.json();
+        console.log(responseJson);
+        return responseJson;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+
+  const debouncedFetchPokemon = useMemo(() => {
+    return debounce((searchTerm) => {
+      fetchPokemon(searchTerm);
+    }, DEBOUNCE_TIME_MILLISECONDS);
+  }, []);
+
+
+  useEffect(() => {
+    if(searchText) {
+      debouncedFetchPokemon(searchText);
+    }
+
+  } ,[searchText, debouncedFetchPokemon]);
   // en el return usamos JSX, que es como HTML dentro de JS
   return (
     // JSX solo soporta enviar un solo elemento, así que todo debe de estar dentro de un fragment, como mínimo
@@ -15,7 +43,7 @@ function App() {
       <header>
         Pokedexito 2026        
       </header>
-      <label for="pokemon">Escribe el nombre del pokemon aquí</label>
+      <label htmlFor="pokemon">Escribe el nombre del pokemon aquí</label>
       <input type="text" placeholder="eevee" name="pokemon" onChange={handleSearchboxChange}></input>
     </>
   );
