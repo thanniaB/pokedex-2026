3e5d5a3d7063eea57517cb801f17c23461051fdd
tblanchet <thannia.blanchet@points.com>
2026-09-18 15:23:41 -0400

    paso 1: agregar input

---
+++ pokedex-2026/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        Pokedexito 2026        
+      </header>
+      <input type="text" placeholder="escribe el nombre del pokemon aquí"></input>
+    </>
+  );
+}
+
+export default App;
